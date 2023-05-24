f7adc1f869821e71f4d57a92edeba0dd817e8572
light-dawn <1790969609@qq.com>
2023-05-24 19:06:07 +0800

    fixing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import ListLayout from '@/components/ListLayout'
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+
 export function NavBar() {
     return <>
         <div className="navbar bg-base-100">
@@ -8,9 +9,11 @@ export function NavBar() {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link href="/">Blog</Link></li>
-                    <li><Link href="/project">Project</Link></li>
-                    <li><Link href="/about">About</Link></li>
+                    <li key={0}><Link href="/">博客</Link></li>
+                    <li key={1}><Link href="/projects">项目</Link></li>
+                    <li key={2}><Link href="/papers">论文</Link></li>
+                    <li key={3}><Link href="/assets">知识产权</Link></li>
+                    <li key={4}><Link href="/about">关于我</Link></li>
                 </ul>
             </div>
         </div>
