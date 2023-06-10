fd94e4e738f851aeb9d63fd33e3cb4330e704e9e
light-dawn <1790969609@qq.com>
2023-06-11 00:40:49 +0800

    fix.

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,6 @@ import { RouterProvider } from "react-router-dom";
 import '@/css/main.css'
 import { router } from './router';
 
-import {
-  Carousel,
-  initTE,
-} from "tw-elements";
-
-
-initTE({ Carousel });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
