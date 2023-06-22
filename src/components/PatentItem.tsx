11103667d0652c5688fd5ff436b9f5e2b0558cb9
light-dawn <1790969609@qq.com>
2023-06-22 19:22:53 +0800

    patent contents.

--- src/components/PatentItem.tsx
+++ src/components/PatentItem.tsx
@@ -7,21 +7,25 @@ type PatentItemProps = {
     publicID: string,
     publicDate: string,
     authorOrder: string,
-    abstract: string
+    abstract: string,
+    isEmpowered: boolean
 }
 
 
 export const PatentItem: React.FC<PatentItemProps> = (props: PatentItemProps) => {
+
+    const empower = props.isEmpowered ? "（授权）" : ""
+
     return (
         <div className="card card-side bg-base-100 shadow-2xl p-12">
             <div className="grid grid-cols-4">
                 <figure className="w-auto"><img src={props.thumbnail} alt="Movie"/></figure>
                 <div className="card-body col-span-3">
-                    <p className="text-2xl font-bold">{props.title}</p>
-                    <p className="text-green-600">公开号：{props.publicID}</p>
-                    <p className="text-green-600">公开日：{props.publicDate}</p>
-                    <p className="text-green-600">发明人顺位：{props.authorOrder}</p>
-                    <p className="text-green-600">摘要：{props.abstract}</p>
+                    <p className="text-2xl font-bold">{props.title}{empower}</p>
+                    <p className="text-green-600">公开号{empower}：{props.publicID}</p>
+                    <p className="text-green-600">公开日{empower}：{props.publicDate}</p>
+                    <p className="text-green-600">发明人顺位{empower}：{props.authorOrder}</p>
+                    <p className="text-green-600">摘要{empower}：{props.abstract}</p>
                 </div>
             </div>
         </div>
